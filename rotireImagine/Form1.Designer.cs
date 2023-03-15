021f9d5e7fe6c65eb4808f55b55257b103223cf9
RAZVAN TRUFIN <razvantrufin99x@gmail.com>
2023-03-15 21:42:26 +0200

    Add project files.

---
+++ rotireImagine/Form1.Designer.cs
@@ -0,0 +1,87 @@
+﻿namespace rotireImagine
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackgroundImage = global::rotireImagine.Properties.Resources._8cdcfce193dae07c4a4ef442d3b390fb;
+            this.panel1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.panel1.Location = new System.Drawing.Point(107, 44);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(662, 625);
+            this.panel1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(813, 108);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "to left";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(894, 108);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "to right";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1179, 744);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ rotireImagine/Form1.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace rotireImagine
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+   
+      
+
+        int rotating = 0;
+        Image imageFile;
+        public void rotate()
+        {
+
+            // Create image.
+             imageFile = Image.FromFile("u.jpg");
+
+            // Create graphics object for alteration.
+            Graphics newGraphics = Graphics.FromImage(imageFile);
+
+            //move rotation point to center of image.
+            newGraphics.TranslateTransform((float)imageFile.Width / 2, (float)imageFile.Height / 2);
+            newGraphics.RotateTransform(rotating);
+            newGraphics.TranslateTransform(-(float)imageFile.Width / 2, -(float)imageFile.Height / 2);
+            newGraphics.InterpolationMode = InterpolationMode.HighQualityBicubic;
+
+          
+
+            // Draw image to screen.
+            newGraphics.DrawImage(imageFile, new PointF(0.0F, 0.0F));
+
+            panel1.BackgroundImage = imageFile;
+
+
+        }
+       
+
+       
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            rotating-=1;
+
+            rotate();
+
+
+
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            rotating+=1;
+            rotate();
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+          
+           
+        }
+    }
+}
