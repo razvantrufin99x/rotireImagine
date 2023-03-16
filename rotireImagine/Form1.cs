ae041e103e364f2a3b126e7f0af0de6f7b0a5dff
RAZVAN TRUFIN <razvantrufin99x@gmail.com>
2023-03-16 04:18:43 +0200

    adaugare functii mouse

--- rotireImagine/Form1.cs
+++ rotireImagine/Form1.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Diagnostics.Eventing.Reader;
 using System.Drawing;
 using System.Drawing.Drawing2D;
 using System.Linq;
@@ -18,8 +19,18 @@ namespace rotireImagine
             InitializeComponent();
         }
 
-   
-      
+
+        public bool isMouseDown = false;
+        public int x1, x2, y1, y2, prevx, prevy;
+
+
+        public float distantaintredouapuncte2dxy(float x1, float y1, float x2, float y2)
+        {
+            float c;
+            c = (float)Math.Sqrt(Math.Abs(x1 - x2) * Math.Abs(x1 - x2) + Math.Abs(y1 - y2) * Math.Abs(y1 - y2));
+            return c;
+        }
+
 
         int rotating = 0;
         Image imageFile;
@@ -73,6 +84,35 @@ namespace rotireImagine
         {
           
            
+        }
+
+        private void Form1_MouseDown(object sender, MouseEventArgs e)
+        {
+            isMouseDown = true;
+            x1 = e.X;
+            y1 = e.Y;
+        }
+
+        private void Form1_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (isMouseDown == true) {
+                x2 = e.X;
+                y2 = e.Y;
+                float r = distantaintredouapuncte2dxy(button1.Left, button1.Top, button2.Left, button2.Top);
+                Text = r.ToString();
+                if (prevx > x2) { rotating = rotating - (Math.Abs(prevx - x2)) / 10; }
+                else if (prevx == x2) { }
+                else { rotating = rotating + (Math.Abs(x2 - prevx)) / 10; }
+                prevx = x2;
+                prevy = y2;
+               
+            }
+            rotate();
+        }
+
+        private void Form1_MouseUp(object sender, MouseEventArgs e)
+        {
+            isMouseDown=false;
         }
     }
 }
